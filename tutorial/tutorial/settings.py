1f7a94981ed47be376e03787508d9c20e127654d
andrei <andrei@MacBook-Pro-andrei.local>
2023-01-17 23:35:08 +0400

    ff

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -11,15 +11,15 @@ NEWSPIDER_MODULE = 'tutorial.spiders'
 ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
-CONCURRENT_REQUESTS = 50
+CONCURRENT_REQUESTS = 60
 
 # Configure a delay for requests for the same website (default: 0)
 # See https://docs.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 0
 # The download delay setting will honor only one of:
-CONCURRENT_REQUESTS_PER_DOMAIN = 50
-CONCURRENT_REQUESTS_PER_IP = 50
+CONCURRENT_REQUESTS_PER_DOMAIN = 60
+CONCURRENT_REQUESTS_PER_IP = 60
 
 # Disable cookies (enabled by default)
 #COOKIES_ENABLED = False
