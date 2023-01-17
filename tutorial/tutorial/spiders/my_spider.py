b1c82940d38890fd92785c73d519259cdac6455e
andrei <andrei@MacBook-Pro-andrei.local>
2023-01-17 23:39:21 +0400

    ff

--- tutorial/tutorial/spiders/my_spider.py
+++ tutorial/tutorial/spiders/my_spider.py
@@ -30,7 +30,7 @@ class DusharaSpider(scrapy.Spider):
             first_zip_value = row["4"].split(" to ")[0]
             second_zip_value = row["4"].split(" to ")[-1]
             url = 'https://iwfa.com/wp-admin/admin-ajax.php'
-            for zip_code in range(int(first_zip_value), int(second_zip_value)):
+            for zip_code in list(range(int(first_zip_value), int(second_zip_value)))[:10]:
                 yield scrapy.FormRequest(
                     url=url,
                     formdata={"lat": "0", "long": "0", "zip": str(zip_code), "nonce": "c47ae0f2e4", "action": "find_dealers"},
